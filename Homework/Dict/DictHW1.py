332ddf13f3056c53cee4800c0bda7f515ebe554e
benyakrck <sergrb27@yandex.ru>
2022-07-15 22:09:57 +0300

    hwdict1

--- Homework/Dict/DictHW1.py
+++ Homework/Dict/DictHW1.py
@@ -2,7 +2,6 @@
 [1] Даны два словаря: dictionary_1 = {'a': 300, 'b': 400} и dictionary_2 = {'c': 500, 'd': 600}.
 Объедините их в один при помощи встроенных функций языка Python. Результаты выведите на экран.
 """
-dictionary_1 = {'a': 300, 'b': 400}
-dictionary_2 = {'c': 500, 'd': 600}
-dictionary_1.update(dictionary_2)
+dictionary_1 = {'a': 300, 'b': 400}.update({'c': 500, 'd': 600})
+dictionary_1.update({'c': 500, 'd': 600})
 print(dictionary_1)
