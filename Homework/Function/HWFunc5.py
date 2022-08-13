258ef698e9fefd4ae566f38030bebbea48ed4b41
benyakrck <sergrb27@yandex.ru>
2022-08-13 21:32:54 +0300

    hwfunc1-6

--- Homework/Function/HWFunc5.py
+++ Homework/Function/HWFunc5.py
@@ -2,4 +2,16 @@
 [5] Написать функцию-декоратор, которая вычисляет время выполнения декорируемой функции,
 а также выводит на экран имя функции и ее параметры.
 """
+from datetime import datetime
+import time
+def data_time(func):
+    start_time = datetime.now()
+    time.sleep(5)
 
+    return (datetime.now() - start_time)
+@data_time
+def sum_range(start, end):
+    if start > end:
+        start, end = end, start
+    return sum(range(start, end + 1))
+print(sum_range(4, 1))
