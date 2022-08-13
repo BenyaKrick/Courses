2eb2d54115f85a934a9eef41a7d5861139737449
benyakrck <sergrb27@yandex.ru>
2022-08-13 18:34:50 +0300

    hwfunc1-3

--- Homework/Function/HWFunc1.py
+++ Homework/Function/HWFunc1.py
@@ -2,11 +2,13 @@
 1] Напишите функцию sum_range(start, end), которая суммирует все целые числа от значения «start» до величины
 «end» включительно. Если пользователь задаст первое число большее чем второе, просто поменяйте их местами.
 """
+
+
 def sum_range(start, end):
-    sum = 0
-    if end < start:
-        star, end = end, start
-    for i in range(start, end+1):
-        sum += i
-    return sum
+    if start > end:
+        start, end = end, start
+    return sum(range(start, end + 1))
+
+
+print(sum_range(4, 1))
 print(sum_range(1, 4))
---
+++ Homework/Function/HWFunc3.py
@@ -0,0 +1,13 @@
+"""
+[3] Напишите функцию, чтобы проверить, является ли строка панграммой или нет. Панграмма — фраза,
+содержащая в себе все буквы алфавита.
+"""
+
+
+def text_pangrama(t):
+    return not set('abcdefghijklmnopqrstuvwxyz') - set(t.lower())
+
+
+text = input()
+
+print(text_pangrama(text))
