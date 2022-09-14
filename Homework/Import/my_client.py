70c20fa0cb6f0687439bef91bb6074f6f869d18c
benyakrck <sergrb27@yandex.ru>
2022-09-14 21:48:37 +0300

    first commit my_client

--- Homework/Import/my_client.py
+++ Homework/Import/my_client.py
@@ -1,6 +1,6 @@
 # import my_mod
-import my_pkd.my_mod
 # from my_pkd import my_mod
-
+# from Homework.Import.my_pkd import my_mod
+from Homework.Import.my_pack import my_pkd.my_mod
 
 print(my_mod.test('file.txt'))
