1a10bfc6243b5f96465d9b2ecbc06e0031066cd1
benyakrck <sergrb27@yandex.ru>
2022-09-22 21:50:00 +0300

    first commit my_client

--- Homework/practice/task_6.py
+++ Homework/practice/task_6.py
@@ -2,6 +2,8 @@
 # Напишите цикл for, который выводит элементы
 # словаря в отсортированном порядке (по возрастанию).
 '''
-_dic = {1: 6, 3: 4, 5: 2}
-sorted_pairs = [(k, _dic[k]) for k in sorted(_dic.keys(), key=_dic.get, reverse=True)]
-print(sorted_pairs)
+_dic = {1: 6, 8: 4, 5: 2}
+sorted_pairs = [(k, _dic[k]) for k in sorted(_dic.keys(), key=_dic.get, reverse=False)]
+
+for x in sorted_pairs:
+    print(x[0], x[1])
