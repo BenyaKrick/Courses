8e06fdd8d08a5786e2e5a2b6664a8e0dd61979b8
benyakrck <sergrb27@yandex.ru>
2022-10-09 17:38:57 +0300

    first commit my_client

--- Homework/SQL/sql_hw2.py
+++ Homework/SQL/sql_hw2.py
@@ -12,7 +12,8 @@ sql.execute("""CREATE TABLE IF NOT EXISTS students (
       student_email not null
       )""")
 db.commit()
-
+for i in range(10):
+    id_name = i
 sur_name = input('enter surname: ')
 student_name = input('enter name: ')
 middle_name = input('enter middlename: ')
