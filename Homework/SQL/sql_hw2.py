d11f6533e6711b4b7f2a2893f0b2a90be5f02189
benyakrck <sergrb27@yandex.ru>
2022-10-09 19:10:16 +0300

    first commit my_client

--- Homework/SQL/sql_hw2.py
+++ Homework/SQL/sql_hw2.py
@@ -1,17 +1,22 @@
 import sqlite3
 
 db = sqlite3.connect('server.db')
-sql = db.cursor()
+sql_c = db.cursor()
 
-sql.execute("""CREATE TABLE IF NOT EXISTS students (
-      Id_name integer PRIMARY KEY not null 
-      sur_name TEXT not null, 
-      student_name TEXT not null, 
-      middle_name TEXT not null,
-      group_number INTEGER not null,
-      )""")
+
+def create_table():
+    sql_c.execute("""CREATE TABLE IF NOT EXISTS students (
+          Id_name integer PRIMARY KEY not null 
+          sur_name TEXT not null, 
+          student_name TEXT not null, 
+          middle_name TEXT not null,
+          group_number INTEGER not null,
+          )""")
+
+
 db.commit()
 
+db.close()
 
 # if __name__ == '__main__':
 #     pass
