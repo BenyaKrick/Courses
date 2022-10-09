961d0048fc79c5c83bddfcb296e7ade6fc3b5cb0
benyakrck <sergrb27@yandex.ru>
2022-10-09 20:35:27 +0300

    first commit my_client

---
+++ Homework/SQL/sql_hw3.py
@@ -0,0 +1,12 @@
+from sql_hw2 import create_table
+from sql_hw2 import create_connection
+
+create_connection("server.db")
+
+
+def main():
+    create_connection("server.db")
+
+
+if __name__ == '__main__':
+    main('')
