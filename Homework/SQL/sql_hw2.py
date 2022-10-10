26d67c78c8f8bebdb5e76f544df699d7e07fe1a7
benyakrck <sergrb27@yandex.ru>
2022-10-10 19:02:18 +0300

    first commit my_client

--- Homework/SQL/sql_hw2.py
+++ Homework/SQL/sql_hw2.py
@@ -2,24 +2,3 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection(db_server):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_server)
-        return conn
-    except Error as e:
-        print(e)
-    return conn
-
-
-def create_table(conn, create_table_sql):
-    try:
-        cursor = conn.cursor()
-        cursor.execute(create_table_sql)
-        conn.commit()
-    except Error as e:
-        print(e)
-
-
-if __name__ == '__main__':
-    create_connection('server.db')
